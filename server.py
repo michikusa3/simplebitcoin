46fc522f5e1e43f3dd530529781f33cc79cd0042
奥田紘世 <>
2018-10-08 15:21:09 +0900

    Server socket に Client socket を使って接続してデータを送信する簡易クライアントを作成しました

---
+++ server.py
@@ -0,0 +1,47 @@
+from concurrent.futures import ThreadPoolExecutor
+import socket
+import os
+
+def __handle_message(args_tuple):
+    conn,addr, data_sum = args_tuple
+    while True:
+        data = conn.recv(1024)
+        data_sum = data_sum + data.decode('utf-8')
+
+        if not data:
+            break
+
+        if data_sum != '':
+            print(data_sum)
+
+def __get_myip():
+    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    s.connet(('8.8.8.8',80))
+    return s.getsockname()[0]
+
+def main():
+
+    # AF_INET : IPv4 ベースのアドレス体系を使うこと
+    # SOCK_STREAM : TCP/IPを使うこと
+    my_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    #多重接続担ってもいいようにスレッドで処理する
+    executor = ThreadPoolExecutor(max_worker=os.cpu_count())
+
+    # 開くポート番号は適当に選ぶ
+    myhost = __get_myip()
+    print('my ip address is now ...',myhost)
+    my_socket.bind((myhost,50030))
+    # 同時に接続してくる相手の数。今回はテストなのでとりあえず１
+    my_socket.listen(1)
+
+    while True:
+        # 接続がある迄待機
+        print('Waitng for the connection ...')
+        conn, addr = my_socket.accept()
+        print('Connented by .. ',addr)
+        data_sum = ''
+        executor.submit(__handle_message,(conn,addr,data_sum))
+
+if __name__ == '__main__':
+    main()
